3f6b319c456fd71553776f5860956fae170cbe12
kieraliz <kenavarro@ucsd.edu>
2024-05-20 22:02:48 -0700

    Complete Expose

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,43 +8,16 @@ class RecipeCard extends HTMLElement {
 
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
-    let shadowEl = this.attachShadow({ mode: "open" });
+    let shadowRoot = this.attachShadow({ mode: "open" });
 
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
-    let elementArticle = document.createElement("article");
+    let articleEl = document.createElement("article");
 
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-    let elementStyle = document.createElement("style");
+    let styleEl = document.createElement("style");
 
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made (copy everything INSIDE the <style> tag>)
-    let cardTemplate_string = `<!-- This is a template for you to reference when making your cards.
-You won't need to access this file from your code, this is the code
-you'll copy from when making your template literals or inserting
-styles into the <style> element of your RecipeCard.js-->
-
-<!-- Markup -->
-<!-- FAKE VALUES HAVE BEEN INCLUDED, just for reference, you will need to replace
-	the attribute values and element content with data -->
-<article>
- <img src="https://link-to-article.com/recipe-thumbnail.jpg"
-   alt="Recipe Title">
- <p class="title">
-   <a href="https://link-to-article.com">Title</a>
- </p>
- <p class="organization">The Chef's Organization</p>
- <div class="rating">
-   <span>5</span>
-   <img src="/assets/images/icons/5-star.svg" alt="5 stars">
-   <span>(500)</span>
- </div>
- <time>50 min</time>
- <p class="ingredients">
-   Comma, Separated, List, of, Ingredients
- </p>
-</article>
-
-<!-- Styles -->
-<style>
+    styleEl.innerHTML = `
  * {
    font-family: sans-serif;
    margin: 0;
@@ -120,13 +93,11 @@ styles into the <style> element of your RecipeCard.js-->
    color: #70757A;
    font-size: 12px;
  }
-</style>`;
-
-    elementStyle.innerHTML = cardTemplate_string;
+`;
 
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
-    shadowEl.append(elementArticle);
-    shadowEl.append(elementStyle);
+    shadowRoot.append(styleEl);
+    shadowRoot.append(articleEl);
   }
 
   /**
@@ -155,7 +126,7 @@ styles into the <style> element of your RecipeCard.js-->
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-    let elementArticle = this.shadowRoot.querySelector("article");
+    let articleEl = this.shadowRoot.querySelector("article");
 
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
@@ -164,22 +135,22 @@ styles into the <style> element of your RecipeCard.js-->
     // 			 Do NOT include the <article> tags within the innerHTML of the element you create.
     //           Remember to replace all the placeholders in the template with the data passed in.
     //           i.e. imgSrc, titleLnk, etc
-    elementArticle.innerHTML = `
-<img src="${data.imgSrc}"
-	alt="${data.imgAlt}">
+    articleEl.innerHTML = `
+<img src="${data.imgSrc}"	alt="${data.imgAlt}">
 <p class="title">
     <a href="${data.titleLnk}">${data.titleTxt}</a>
 </p>
 <p class="organization">${data.organization}</p>
 <div class="rating">
     <span>${data.rating}</span>
-    <img src="/assets/images/icons/5-star.svg" alt="5 stars">
-    <span>(500)</span>
+    <img src="./assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+    <span>${data.numRatings}</span>
 </div>
 <time>${data.lengthTime}</time>
 <p class="ingredients">
     ${data.ingredients}
-</p>`;
+</p>
+`;
   }
 }
 
